e4ea9dd306359be8a223dce010b0406fdc354e79
h-adji <h-adji_tech@proton.me>
2023-08-03 13:29:02 +0200

    refactored Cart code and added tests

--- shop.py
+++ shop.py
@@ -9,21 +9,39 @@ class MarketingStrategy(ABC):
 
 
 class BackToTheFutureStrategy(MarketingStrategy):
+    """Back to the future sales strategy
+
+
+    """
+
     two_parts_sale = 0.9
     tree_parts_sale = 0.8
     ordinary_dvd_price = 20
     back_to_the_future_price = 15
 
-    def apply_sale_strategy(self, products: list[str]):
+    def apply_sale_strategy(self, products: list[str]) -> float:
+        """Sale strategy to apply to a list of products
+
+        Args:
+            products (list[str])
+
+        Returns:
+            float: final price after applying a sale strategy
+        """
         products_number = len(products)
         parts = set()
         for product in products:
             if "back to the future" in product.lower():
-                movie_part = re.match(r"\d", product)
+                movie_part = re.search(r"\d", product)
                 if movie_part:
-                    parts.add(int(movie_part))
+                    parts.add(int(movie_part.group()))
+        # how many back to the future sequel are ordered
         bttf_count = len(parts)
-        full_price = (products_number - bttf_count) * 20 + bttf_count * 15
+
+        # full price without any sale strategy applied
+        full_price = (
+            products_number - bttf_count
+        ) * self.ordinary_dvd_price + bttf_count * self.back_to_the_future_price
 
         if bttf_count == 2:
             return full_price * self.two_parts_sale
@@ -33,6 +51,10 @@ class BackToTheFutureStrategy(MarketingStrategy):
 
 
 class Cart:
+    """
+    Cart holding dvd articles
+    """
+
     def __init__(self, products: list[str] = None) -> None:
         self.products = products or []
 
@@ -46,5 +68,9 @@ class Cart:
     def cart_size(self):
         return len(self.products)
 
-    def payment(self, strategy: MarketingStrategy = None):
+    @property
+    def is_empty(self):
+        return self.cart_size == 0
+
+    def price(self, strategy: MarketingStrategy = None):
         return strategy.apply_sale_strategy(self.products)
--- test_cart.py
+++ test_cart.py
@@ -1,6 +1,7 @@
 import pytest
 
-from shop import Cart,BackToTheFutureStrategy
+from shop import BackToTheFutureStrategy, Cart
+
 
 @pytest.fixture
 def random_movies():
@@ -23,7 +24,6 @@ def special_cart_1():
         [
             "Back to the Future 1",
             "Back to the Future 2",
-            "Back to the Future 3",
         ]
     )
 
@@ -49,9 +49,16 @@ def normal_cart():
     )
 
 
-def test_empty_cart(empty_cart: Cart):
+def test_empty_cart(normal_cart: Cart, empty_cart: Cart):
+    assert empty_cart.is_empty
     assert empty_cart.cart_size == 0
     assert empty_cart.products == []
+
+    assert normal_cart.is_empty is False
+    assert normal_cart.cart_size > 0
+    normal_cart.empty_cart()
+    assert normal_cart.cart_size == 0
+    assert normal_cart.products == []
 
 
 def test_random_cart(random_movies: list[str]):
@@ -60,13 +67,29 @@ def test_random_cart(random_movies: list[str]):
     assert my_cart.products == random_movies
 
 
-def test_strategy(normal_cart:Cart):
+def test_strategy(normal_cart: Cart, special_cart_1: Cart, special_cart_2: Cart):
     startegy = BackToTheFutureStrategy()
-    assert normal_cart.payment(strategy=startegy) == normal_cart.cart_size*20
+    assert normal_cart.price(strategy=startegy) == normal_cart.cart_size * 20
+    assert (
+        special_cart_1.price(strategy=startegy)
+        == (
+            (special_cart_1.cart_size - 2) * startegy.ordinary_dvd_price
+            + 2 * startegy.back_to_the_future_price
+        )
+        * startegy.two_parts_sale
+    )
+    assert (
+        special_cart_2.price(strategy=startegy)
+        == (
+            (special_cart_2.cart_size - 3) * startegy.ordinary_dvd_price
+            + 3 * startegy.back_to_the_future_price
+        )
+        * startegy.tree_parts_sale
+    )
 
-def test_add_products():
-    pass
 
-
-def test_remove_products():
-    pass
+def test_add_products(empty_cart: Cart):
+    assert empty_cart.is_empty
+    empty_cart.add_dvd("back to the future 5")
+    empty_cart.add_dvd("Barbie")
+    assert empty_cart.cart_size == 2
